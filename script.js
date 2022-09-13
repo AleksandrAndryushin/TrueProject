d86b1dd9904ec94c00a2eeec0d48cba206443a88
Aleksandr <aleksandr-andrushin@yandex.ru>
2022-09-13 14:43:00 +0300

    added changes

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-'use strict';
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-let number = 23;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+let a = prompt("Один из просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
