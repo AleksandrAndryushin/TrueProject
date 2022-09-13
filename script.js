fbe613c90d9ee4351e1b946cbf3b2a79c0a216d4
Aleksandr <aleksandr-andrushin@yandex.ru>
2022-09-13 16:31:03 +0300

    Added logical operators

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-if (4 == 5) {
-    console.log('ok!');
-} else {
-    console.log('Error!');
-}
+console.log(NaN || 2 || undefined);
 
+console.log(NaN && 2 && undefined);
 
-// if (num < 49) {
-//     console.log('Error!');
-// } else if (num > 100) {
-//     console.log('Много');
-// } else {
-//     console.log('ok!')
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 && !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log( NaN || null && !3 && undefined || 5);
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
 // }
 
-// (num === 50) ? console.log('ok!') : console.log('Error!');
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!');
+// }
 
-const num = 50;
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
 
-switch (num) {
-    case 49:
-        console.log('Неверно!');
-        break;
-    case 100:
-        console.log('Неверно!');
-        break;
-    case '50':
-        console.log('Правда!');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
-}
