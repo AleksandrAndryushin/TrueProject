a9ad32f40e44beb63b9dfaa854abf0b58d18643a
Aleksandr <aleksandr-andrushin@yandex.ru>
2022-09-13 15:07:33 +0300

    added condition

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+if (4 == 5) {
+    console.log('ok!');
+} else {
+    console.log('Error!');
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-let a = prompt("Один из просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('ok!')
+// }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// (num === 50) ? console.log('ok!') : console.log('Error!');
 
-console.log(personalMovieDB);
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно!');
+        break;
+    case 100:
+        console.log('Неверно!');
+        break;
+    case '50':
+        console.log('Правда!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
